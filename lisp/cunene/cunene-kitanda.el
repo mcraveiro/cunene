645e83a83ac5dfdece393e1c6fa8008e9910a5b5
Marco Craveiro <marco.craveiro@gmail.com>
2010-07-03 11:22:49 +0100

    - fixes to kitanda c++ file generator (spacing, etc.).

--- lisp/cunene/cunene-kitanda.el
+++ lisp/cunene/cunene-kitanda.el
@@ -39,26 +39,22 @@
  *
 ")
 
-(defun kitanda-new-header ()
+(defun kitanda-new-header()
   "skeleton for a new header file"
   (interactive)
   (insert kitanda-file-variables)
   (kitanda-add-license)
-  (kitanda-add-file-description)
-  (insert "\n")
+  ;; (kitanda-add-file-description)
   (kitanda-add-include-guard)
   (kitanda-add-sample-header)
-  (insert "\n")
   (kitanda-add-namespace))
 
-(defun kitanda-new-source ()
+(defun kitanda-new-source()
   "skeleton for a new source file"
   (interactive)
   (insert kitanda-file-variables)
   (kitanda-add-license)
-  (insert "\n")
   (kitanda-add-sample-header)
-  (insert "\n")
   (kitanda-add-namespace))
 
 ;; key bindings
@@ -80,7 +76,7 @@
               '("boost"
                 "shared_ptr" "format")))
 
-(defun kitanda-add-license ()
+(defun kitanda-add-license()
   (let ((holder (read-from-minibuffer "Copyright holder? "
                                       "Marco Craveiro")))
     (let ((copyright-notice
@@ -89,36 +85,35 @@
                                   (substring (current-time-string) -4)
                                   " "
                                   holder
-                                  "\n"
                                   ()))))
-      (insert "\n"
-              copyright-notice
+      (insert copyright-notice
               kitanda-license
-              "*/\n\n"))))
+              " */\n"))))
 
-(defun kitanda-add-file-description ()
+(defun kitanda-add-file-description()
   (let ((filename (buffer-name))
         (description (read-from-minibuffer "Short file description? ")))
     (insert "/** @file " filename "\n"
             " *  @brief " description "\n"
             " */\n")))
 
-(defun kitanda-add-include-guard ()
+(defun kitanda-add-include-guard()
   (let ((guard (read-from-minibuffer "Include guard? " "KITANDA_")))
     (insert "#ifndef " guard "\n"
             "#define " guard "\n"
-            "\n\n\n"
+            "\n"
+            "#if defined(_MSC_VER) && (_MSC_VER >= 1200)\n"
+            "#pragma once\n"
+            "#endif\n\n\n"
             "#endif\n"))
-  (previous-line 3))
+  (previous-line 2))
 
-(defun kitanda-add-sample-header ()
-  (insert "#include <kitanda/>\n"))
+(defun kitanda-add-sample-header()
+  (insert "#include \"kitanda/\"\n\n"))
 
-
-(defun kitanda-add-namespace ()
+(defun kitanda-add-namespace()
   (insert "namespace kitanda {\n"
-          "\n\n\n"
+          "\n\n"
           "}\n")
   (previous-line 3)
   (c-indent-command))
-
