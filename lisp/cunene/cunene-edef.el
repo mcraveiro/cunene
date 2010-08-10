aad592b83096822d98d0680595d176a9eba96dce
Marco Craveiro <marco.craveiro@gmail.com>
2010-08-10 08:00:06 +0100

    - added edef mode to edit kitanda codegen files.

---
+++ lisp/cunene/cunene-edef.el
@@ -0,0 +1,34 @@
+;;; Marco's .emacs, copied largely from starterkit and Alex Ott's.
+
+;; Copyright (C) 2009  Marco Craveiro
+;;
+;; cunene is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; Cunene is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with cunene.  If not, see <http://www.gnu.org/licenses/>.
+
+(require 'generic-x)
+
+(define-generic-mode
+  'edef-mode
+  '()
+  '("class" "name" "comment" "key_attribute" "class_attribute"
+    "type" "begin" "end")
+  '(
+    ("\\(#.*\\)" 1 'font-lock-comment-face)
+    ("unsigned" . 'font-lock-type-face)
+    ("int" . 'font-lock-type-face)
+    ("std::string" . 'font-lock-type-face)
+    )
+  '("\\edef$")
+  nil
+  "A mode for kitanda's EDEF files."
+  )
