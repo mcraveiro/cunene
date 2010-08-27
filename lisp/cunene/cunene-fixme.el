16a728866a6ea625fa164d92b061382ecae9da0f
Marco Craveiro <marco.craveiro@gmail.com>
2010-08-27 23:19:17 +0100

    - updated GPL notice in cunene lisp files again to make them easier to   maintain.

--- lisp/cunene/cunene-fixme.el
+++ lisp/cunene/cunene-fixme.el
@@ -2,18 +2,18 @@
 
 ;; Copyright (C) 2009  Marco Craveiro
 ;;
-;; cunene is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
 ;;
-;; Cunene is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-;; License for more details.
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with cunene.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Make FIXME stand out
 (setq fixme-modes
