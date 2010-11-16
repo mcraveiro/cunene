84e8655459c24bc868cd6a7d3aa57c3306657a54
Marco Craveiro <marco.craveiro@gmail.com>
2010-11-16 22:57:36 +0000

    - delete buffer and file. source: http://blog.tuxicity.se/

--- lisp/cunene/cunene-misc.el
+++ lisp/cunene/cunene-misc.el
@@ -297,6 +297,21 @@ text (with prefix arg don't indent)."
 
 (global-set-key (kbd "C-c r") 'rename-file-and-buffer)
 
+(defun delete-file-and-buffer ()
+  "Deletes current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-c k") 'delete-this-buffer-and-file)
+
 ;;
 ;; no emacs is complete without the power of the butterfly!
 ;;
