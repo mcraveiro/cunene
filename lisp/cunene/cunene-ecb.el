6738db6cba60b18c3e3383cce7abaf2f5c84c278
Marco Craveiro <marco.craveiro@gmail.com>
2011-10-05 07:11:44 +0100

    stop the annoying message on ecb activate.

--- lisp/cunene/cunene-ecb.el
+++ lisp/cunene/cunene-ecb.el
@@ -18,11 +18,25 @@
 ;; Add ecb to load path.
 (add-to-list 'load-path (concat dotfiles-dir "/other/ecb"))
 
-;; Emacs code browser
-(require 'ecb)
-
 ;; Version
 (setq ecb-options-version "2.40")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Emacs code browser
+(require 'ecb)
 
 ;; Do not show tip of the day
 (setq ecb-tip-of-the-day nil)
