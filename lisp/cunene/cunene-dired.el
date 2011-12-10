3f2ad8e996dc5339c236e8c29072da3c0c8832c9
Marco Craveiro <marco.craveiro@gmail.com>
2011-12-10 23:31:51 +0000

    dired: ediff marked files.

--- lisp/cunene/cunene-dired.el
+++ lisp/cunene/cunene-dired.el
@@ -37,3 +37,15 @@
     (my-dired-init)
   ;; it's not loaded yet, so add our bindings to the load-hook
   (add-hook 'dired-load-hook 'my-dired-init))
+
+(defun dired-ediff-marked-files ()
+  "Run ediff on marked ediff files."
+  (interactive)
+  (set 'marked-files (dired-get-marked-files))
+  (when (= (safe-length marked-files) 2)
+    (ediff-files (nth 0 marked-files) (nth 1 marked-files)))
+
+  (when (= (safe-length marked-files) 3)
+    (ediff3 (buffer-file-name (nth 0 marked-files))
+            (buffer-file-name (nth 1 marked-files))
+            (buffer-file-name (nth 2 marked-files)))))
