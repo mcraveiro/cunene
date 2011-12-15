d408fec7d9c173f0b96de28d30c89b01083fa468
Marco Craveiro <marco.craveiro@gmail.com>
2011-12-15 22:19:50 +0000

    fringe: enable rfringe mode

--- config/oracle.sql
+++ config/oracle.sql
@@ -3,13 +3,13 @@ select cast(sys_context('userenv', 'current_user') AS varchar2(30)) current_user
        cast(sys_context('userenv', 'session_user') AS varchar2(30)) session_user,
        cast(sys_context( 'userenv', 'current_schema') AS varchar2(30)) current_schema
 from dual;
- 
+
 -- all tables for current schema
 select table_name
 from all_tables
 where owner = sys_context('userenv', 'current_schema')
 order by table_name;
- 
+
 -- list fields for table
 select cast(column_name AS varchar2(30)) column_name, cast(data_type as varchar2(50)) data_type
 from all_tab_cols
