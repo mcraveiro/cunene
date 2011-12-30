ad39369f8a66fc757567fb8b320edcec525a6c26
Marco Craveiro <marco.craveiro@gmail.com>
2011-12-30 00:36:51 +0000

    bongo: update from latest in git.

--- lisp/other/bongo/bongo-lastfm.el
+++
@@ -1,8 +0,0 @@
-;;; bongo-lastfm.el --- submit information to Last.fm from Bongo
-
-;; Author: Daniel Brockman <daniel@brockman.se>
-;; URL: http://www.brockman.se/software/bongo/bongo-lastfm.el
-;; Created: May 24, 2006
-;; Updated: November 11, 2006
-
-;; This file was merged into `bongo.el' on November 11, 2006.
