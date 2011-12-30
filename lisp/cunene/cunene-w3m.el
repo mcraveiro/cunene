72ea4deb53fe2824bbbfa21782e9b1e85fc70996
Marco Craveiro <marco.craveiro@gmail.com>
2011-12-30 00:59:44 +0000

    w3m: add web pages to desktop-save

--- lisp/cunene/cunene-w3m.el
+++ lisp/cunene/cunene-w3m.el
@@ -85,12 +85,23 @@
 ;;           (lambda ()
 ;;             (local-set-key "\M-W" 'w3m-copy-url-at-point)))
 
-(defun my-w3m-rename-buffer (url)
-  "Renames the current buffer to be the current URL"
-  (rename-buffer url t))
-(add-hook 'w3m-display-hook 'my-w3m-rename-buffer)
+;; save state
+(defun w3m-register-desktop-save ()
+  "Set `desktop-save-buffer' to a function returning the current URL."
+  (setq desktop-save-buffer (lambda (desktop-dirname) w3m-current-url)))
 
-(add-hook 'w3m-display-hook
-          (lambda (url)
-            (let ((buffer-read-only nil))
-              (delete-trailing-whitespace))))
+(add-hook 'w3m-mode-hook 'w3m-register-desktop-save)
+
+(defun w3m-restore-desktop-buffer (d-b-file-name d-b-name d-b-misc)
+  "Restore a `w3m' buffer on `desktop' load."
+  (when (eq 'w3m-mode desktop-buffer-major-mode)
+    (let ((url d-b-misc))
+      (when url
+        (require 'w3m)
+        (if (string-match "^file" url)
+            (w3m-find-file (substring url 7))
+          (w3m-goto-url-new-session url))
+        (current-buffer)))))
+
+(add-to-list 'desktop-buffer-mode-handlers
+             '(w3m-mode . w3m-restore-desktop-buffer))
