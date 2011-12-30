8800588c92ad1fe00d3e30035eb20368c9b9ab0e
Marco Craveiro <marco.craveiro@gmail.com>
2011-12-30 00:13:25 +0000

    fixme: activate on org-mode too

--- lisp/cunene/cunene-fixme.el
+++ lisp/cunene/cunene-fixme.el
@@ -17,7 +17,8 @@
 
 ;; Make FIXME stand out
 (setq fixme-modes
-      '(latex-mode makefile-mode c++-mode emacs-lisp-mode sh-mode text-mode))
+      '(latex-mode makefile-mode c++-mode emacs-lisp-mode sh-mode text-mode
+                   org-mode))
 (make-face 'font-lock-fixme-face)
 (mapc (lambda (mode)
         (font-lock-add-keywords
