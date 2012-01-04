9cf656487e2ed744b625f9346cbd9c04164dc5dc
Marco Craveiro <marco.craveiro@gmail.com>
2012-01-04 21:52:29 +0000

    org: fix tab expansion with org and yas

--- lisp/cunene/cunene-org.el
+++ lisp/cunene/cunene-org.el
@@ -120,3 +120,14 @@ Skips capture tasks."
    (sh . t)
    (gnuplot . t)
    (R . t)))
+
+(defun yas/org-very-safe-expand ()
+  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; yasnippet (using the new org-cycle hooks)
+            (make-variable-buffer-local 'yas/trigger-key)
+            (setq yas/trigger-key [tab])
+            (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+            (define-key yas/keymap [tab] 'yas/next-field)))
