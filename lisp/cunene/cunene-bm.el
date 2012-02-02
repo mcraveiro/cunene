1ea83842e60e5d96118b446f138ad850a9f01a78
Marco Craveiro <marco.craveiro@gmail.com>
2012-02-02 22:48:42 +0000

    bm: update configuration
    - add persistent bookmarks
    - recenter when adding a bookmark

--- lisp/cunene/cunene-bm.el
+++ lisp/cunene/cunene-bm.el
@@ -18,11 +18,42 @@
 ;; Visual bookmarks mode
 (require 'bm)
 
+;; location of the persistent bookmarks
+(setq bm-repository-file (concat datafiles-dir "/bm-repository"))
+
 ;; Put bookmarks on the fringe
 (setq bm-highlight-style 'bm-highlight-only-fringe)
 
 ;; cycle across buffers
 (setq bm-cycle-all-buffers t)
+
+;; repository should be restored when loading `bm'
+(setq bm-restore-repository-on-load t)
+
+;; buffer should be recentered around the bookmark
+(setq bm-recenter t)
+
+;; make bookmarks persistent as default
+(setq-default bm-buffer-persistence t)
+
+;; loading the repository from file when on start up
+(add-hook' after-init-hook 'bm-repository-load)
+
+;; restoring bookmarks when on file find
+(add-hook 'find-file-hooks 'bm-buffer-restore)
+
+;; saving bookmark data on killing a buffer
+(add-hook 'kill-buffer-hook 'bm-buffer-save)
+
+;; saving the repository to file when on exit
+;; `kill-buffer-hook' is not called when emacs is killed, so we
+;; must save all bookmarks first
+(add-hook 'kill-emacs-hook '(lambda nil
+                              (bm-buffer-save-all)
+                              (bm-repository-save)))
+
+;; update bookmark repository when saving the file
+(add-hook 'after-save-hook 'bm-buffer-save)
 
 ;;
 ;; Key bindings
