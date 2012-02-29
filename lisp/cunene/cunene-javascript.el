7616e303f81b55823484bf071781938c6ab81c4f
Marco Craveiro <marco.craveiro@gmail.com>
2012-02-29 23:57:42 +0000

    javascript: added json support including pretty print
    Files found at:
    
    - https://github.com/thorstadt/json.el
    - https://github.com/thorstadt/json-pretty-print.el

--- lisp/cunene/cunene-javascript.el
+++ lisp/cunene/cunene-javascript.el
@@ -17,3 +17,6 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+
+
+(require 'json-pretty-print)
