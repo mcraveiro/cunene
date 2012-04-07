00a2720acacac1c1c1ef03ce52d9a6799470575f
Marco Craveiro <marco.craveiro@gmail.com>
2012-04-08 00:15:36 +0100

    folding: add it to shell mode

--- lisp/cunene/cunene-folding.el
+++ lisp/cunene/cunene-folding.el
@@ -15,4 +15,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+(require 'folding)
+(setq safe-local-variable-values (quote ((folded-file . t))))
+(folding-add-to-marks-list 'sh-mode "# {{{" "# }}}")
 (folding-mode-add-find-file-hook)
