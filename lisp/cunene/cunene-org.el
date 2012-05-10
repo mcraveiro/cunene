172743fddd1d45e0b63b3a0e3bd9af1a7b09cc5a
Marco Craveiro <marco.craveiro@gmail.com>
2012-05-10 07:26:34 +0100

    org: update faces for TODO lists

--- lisp/cunene/cunene-org.el
+++ lisp/cunene/cunene-org.el
@@ -142,3 +142,11 @@ Skips capture tasks."
             (setq yas/trigger-key [tab])
             (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
             (define-key yas/keymap [tab] 'yas/next-field)))
+
+(setq org-todo-keyword-faces
+      '(
+        ("IMPLEMENTATION" . (:foreground "yellow" :weight bold))
+        ("TESTING" . (:foreground "yellow" :weight bold))
+        ("COMPLETED" . (:foreground "green" :weight bold))
+        ("CANCELLED" . (:foreground "red" :weight bold))
+        ))
