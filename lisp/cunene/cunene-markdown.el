c1b33810664ebab751213c8d1d4422267a6e509b
Marco Craveiro <marco.craveiro@gmail.com>
2012-05-23 12:50:38 +0100

    markdown: add mdown extension

--- lisp/cunene/cunene-markdown.el
+++ lisp/cunene/cunene-markdown.el
@@ -19,4 +19,5 @@
 
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
 
