bafec5f4b165e3269f80620112087b7282755880
Marco Craveiro <marco.craveiro@gmail.com>
2012-06-01 08:27:25 +0100

    js2: improve indentation
    source: http://feeding.cloud.geek.nz/2012/05/proper-indentation-of-javascript-files.html

--- lisp/cunene/cunene-javascript.el
+++ lisp/cunene/cunene-javascript.el
@@ -18,5 +18,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
+(setq js2-basic-offset 2)
+(setq js2-bounce-indent-p t)
 
 (require 'json-pretty-print)
