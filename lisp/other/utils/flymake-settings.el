951e228c24f59a69fcc0a5f90fa0812554d3cbf1
Marco Craveiro <marco.craveiro@gmail.com>
2012-06-02 20:56:24 +0100

    flymake: save temp files in temporary directory
    We're seeing a lot of "race conditions" between CMake and flymake. To
    avoid this, try placing flymake temporary files in /tmp. This is not
    ideal because some C++ files actually include other files in the
    current directory.

--- lisp/other/utils/flymake-settings.el
+++ lisp/other/utils/flymake-settings.el
@@ -19,6 +19,33 @@
 ;; write  to  the Free  Software  Foundation,  Inc., 51  Franklin
 ;; Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+(defun flymake-create-temp-intemp (file-name prefix)
+  "Return file name in temporary directory for checking FILE-NAME.
+This is a replacement for `flymake-create-temp-inplace'. The
+difference is that it gives a file name in
+`temporary-file-directory' instead of the same directory as
+FILE-NAME.
+
+For the use of PREFIX see that function.
+
+Note that not making the temporary file in another directory
+\(like here) will not if the file you are checking depends on
+relative paths to other files \(for the type of checks flymake
+makes)."
+  (unless (stringp file-name)
+    (error "Invalid file-name"))
+  (or prefix
+      (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext))
+         )
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
+
 (defvar flymake-mode-map (make-sparse-keymap))
 
 (autoload 'flymake-find-file-hook "flymake" "" t)
@@ -75,7 +102,7 @@ Use CREATE-TEMP-F for creating temp copy."
       args))
 
   (defun flymake-simple-make-gcc-init ()
-    (flymake-simple-make-gcc-init-impl 'flymake-create-temp-inplace t t "Makefile" 'flymake-get-make-gcc-cmdline))
+    (flymake-simple-make-gcc-init-impl 'flymake-create-temp-intemp t t "Makefile" 'flymake-get-make-gcc-cmdline))
 
   (setq flymake-allowed-file-name-masks
         '(("\\.\\(?:c\\(?:pp\\|xx\\|\\+\\+\\)?\\|CC\\)\\'" flymake-simple-make-gcc-init)
