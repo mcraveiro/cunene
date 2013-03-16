f56bac503c0c3aeb28c46250e5392a48601b0223
Marco Craveiro <marco.craveiro@gmail.com>
2013-03-16 21:08:11 +0000

    git gutter: add new mode

---
+++ lisp/cunene/cunene-git-gutter.el
@@ -0,0 +1,32 @@
+;;; Marco's .emacs, copied largely from starterkit and Alex Ott's.
+
+;; Copyright (C) 2009  Marco Craveiro
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(require 'git-gutter)
+
+(global-git-gutter-mode t)
+
+;; always show the gutter
+(setq git-gutter:always-show-gutter t)
+
+(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+
+;; Revert current hunk
+(global-set-key (kbd "C-x r") 'git-gutter:revert-hunk)
