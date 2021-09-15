72176adbe3682b3cf8fc4576b7c8b20e2e1e9b28
Marco Craveiro <marco.craveiro@gmail.com>
2021-09-15 09:01:35 +0100

    add elpa directory

---
+++ elpa/git-gutter-20210730.429/git-gutter-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from git-gutter-20210730.429/git-gutter.el  -*- no-byte-compile: t -*-
+(define-package "git-gutter" "20210730.429" "Port of Sublime Text plugin GitGutter" '((emacs "24.3")) :commit "1003c8cbe2367482ad02422ace0a85a7d56d01d3" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Neil Okamoto" . "neil.okamoto+melpa@gmail.com") :url "https://github.com/emacsorphanage/git-gutter")
