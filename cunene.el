ef3055fac1bc93ec0708ef6fbc2f097260e8695b
Marco Craveiro <marco.craveiro@gmail.com>
2021-09-17 11:55:30 +0100

    add bookmarks support

--- cunene.el
+++ cunene.el
@@ -759,6 +759,90 @@ ARGUMENT determines the visible heading."
   :config
   (global-undo-tree-mode 1))
 
+(use-package bm
+  :ensure t
+  :demand t
+
+  :init
+  ;; restore on load (even before you require bm)
+  (setq bm-restore-repository-on-load t)
+
+  :config
+  ;; Allow cross-buffer 'next'
+  (setq bm-cycle-all-buffers t)
+
+  ;; where to store persistant files
+  (setq bm-repository-file (cunene/cache-concat "bm/bm-repository"))
+  
+  ;; show bookmark in fringe only.
+  (setq bm-highlight-style 'bm-highlight-only-fringe)
+  ;; save bookmarks
+  (setq-default bm-buffer-persistence t)
+
+  ;; Loading the repository from file when on start up.
+  (add-hook 'after-init-hook 'bm-repository-load)
+
+  ;; Saving bookmarks
+  (add-hook 'kill-buffer-hook #'bm-buffer-save)
+
+  ;; Saving the repository to file when on exit.
+  ;; kill-buffer-hook is not called when Emacs is killed, so we
+  ;; must save all bookmarks first.
+  (add-hook 'kill-emacs-hook #'(lambda nil
+                                 (bm-buffer-save-all)
+                                 (bm-repository-save)))
+
+  ;; The `after-save-hook' is not necessary to use to achieve persistence,
+  ;; but it makes the bookmark data in repository more in sync with the file
+  ;; state.
+  (add-hook 'after-save-hook #'bm-buffer-save)
+
+  ;; Restoring bookmarks
+  (add-hook 'find-file-hooks #'bm-buffer-restore)
+  (add-hook 'after-revert-hook #'bm-buffer-restore)
+
+  ;; The `after-revert-hook' is not necessary to use to achieve persistence,
+  ;; but it makes the bookmark data in repository more in sync with the file
+  ;; state. This hook might cause trouble when using packages
+  ;; that automatically reverts the buffer (like vc after a check-in).
+  ;; This can easily be avoided if the package provides a hook that is
+  ;; called before the buffer is reverted (like `vc-before-checkin-hook').
+  ;; Then new bookmarks can be saved before the buffer is reverted.
+  ;; Make sure bookmarks is saved before check-in (and revert-buffer)
+  (add-hook 'vc-before-checkin-hook #'bm-buffer-save)
+
+  :bind (("<f9>" . bm-toggle)
+         ("S-<f9>" . bm-previous)
+         ("C-<f9>" . bm-next)))
+
+(defvar bm-after-goto-hook nil
+  "Hook run after jumping to a bookmark in `bm-goto'.")
+
+(add-hook 'bm-after-goto-hook 'org-bookmark-jump-unhide)
+
+(defun bm-goto (bookmark)
+  "Goto specified BOOKMARK."
+  (if (bm-bookmarkp bookmark)
+      (progn
+        (if bm-goto-position
+            (goto-char (max
+                        ;; sometimes marker-position is before start of overlay
+                        ;; marker is not updated when overlay hooks are called.
+                        (overlay-start bookmark)
+                        (marker-position (overlay-get bookmark 'position))))
+          (goto-char (overlay-start bookmark)))
+        (run-hooks 'bm-after-goto-hook)
+        (setq bm-wrapped nil)           ; turn off wrapped state
+        (if bm-recenter
+            (recenter))
+        (let ((annotation (overlay-get bookmark 'annotation)))
+          (if annotation
+              (message annotation)))
+        (when  (overlay-get bookmark 'temporary-bookmark)
+          (bm-bookmark-remove  bookmark)))
+    (when (> bm-verbosity-level 0)
+      (message "Bookmark not found."))))
+
 (use-package git-commit
   :hook
   (git-commit-mode . (lambda () (setq-local fill-column 72))))
