c875407e92010681c8cd8d1ba89ba805e415df8b
Marco Craveiro <marco.craveiro@gmail.com>
2021-09-23 22:01:57 +0100

    lsp-treemacs: local fix to text scale
    As per discussion in [1]. Will be obtained from upstream when PR is merged.
    
    [1] https://github.com/emacs-lsp/lsp-treemacs/issues/112

--- elpa/lsp-treemacs-20210904.2039/lsp-treemacs.el
+++ elpa/lsp-treemacs-20210904.2039/lsp-treemacs.el
@@ -930,8 +930,7 @@ will be rendered an empty line between them."
       (lsp-treemacs--set-mode-line-format search-buffer title)
       (lsp-treemacs-generic-refresh)
       (when treemacs-text-scale
-        (text-scale-adjust 0)
-        (text-scale-increase treemacs-text-scale))
+        (text-scale-set treemacs-text-scale))
       (when expand-depth (lsp-treemacs--expand 'LSP-Generic expand-depth))
       (current-buffer))))
 
