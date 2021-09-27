d09bfccbcdbc5742bc74371d852943b1ade46fb0
Marco Craveiro <marco.craveiro@gmail.com>
2021-09-27 12:46:16 +0100

    backup-walker: add new mode

---
+++ elpa/backup-walker-20130720.1516/backup-walker-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from backup-walker-20130720.1516/backup-walker.el  -*- no-byte-compile: t -*-
+(define-package "backup-walker" "20130720.1516" "quickly traverse all backups of a file" 'nil :commit "934a4128c122972ac32bb9952addf279a60a94da" :authors '(("Le Wang")) :maintainer '("Le Wang") :keywords '("backup") :url "https://github.com/lewang/backup-walker")
