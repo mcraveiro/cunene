508c0017f518b72425acf83ca40ef641a9454410
Marco Craveiro <marco.craveiro@gmail.com>
2024-02-28 17:59:54 +0000

    site: revert back to external CSS

--- .build-site.el
+++ .build-site.el
@@ -49,7 +49,7 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-(defvar html-header "<link id=\"pagestyle\" rel=\"stylesheet\" type=\"text/css\" href=\"https://raw.githubusercontent.com/mcraveiro/cunene/master/assets/css/org.css\"/>
+(defvar html-header "<link id=\"pagestyle\" rel=\"stylesheet\" type=\"text/css\" href=\"https://gongzhitaao.org/orgcss/org.css\"/>
 <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/highlight.min.js\"></script>
 <script>var hlf=function(){Array.prototype.forEach.call(document.querySelectorAll(\"pre.src\"),function(t){var e;e=t.getAttribute(\"class\"),e=e.replace(/src-(\w+)/,\"src-$1 $1\"),console.log(e),t.setAttribute(\"class\",e),hljs.highlightBlock(t)})};addEventListener(\"DOMContentLoaded\",hlf);</script>
 <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/googlecode.min.css\" />")
