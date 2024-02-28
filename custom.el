a48272272b2f95b7e017d3da25f09d13829f4d90
Marco Craveiro <marco.craveiro@gmail.com>
2024-02-28 17:23:49 +0000

    features: add configuration file

--- custom.el
+++ custom.el
@@ -14,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 5.0)))))
