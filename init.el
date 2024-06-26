35db08609e6fac251a549026c85334e728f5248a
Marco Craveiro <marco.craveiro@gmail.com>
2024-06-26 16:30:49 +0100

    dev: add docker mode

--- init.el
+++ init.el
@@ -162,3 +162,22 @@ ORG-FILE-NAME file to operate on."
   ;; Collect garbage when all else is done
   (garbage-collect))
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(docker ztree yasnippet-snippets yaml-mode windswap vscode-icon volatile-highlights vertico verb undo-tree treemacs-tab-bar treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired terraform-mode super-save ssh sql-clickhouse smartparens smart-hungry-delete sharper rg rainbow-mode rainbow-delimiters protobuf-mode project-shells prodigy powershell persistent-scratch pcmpl-args paimon org-roam-ui org-present org-fancy-priorities orderless nerd-icons-corfu mustache-mode mustache msgu mastodon markdown-mode marginalia logview llama-cpp jump-tree json-mode jq-mode jq-format inf-mongo inf-clojure imenu-list iedit ibuffer-vc ibuffer-sidebar ibuffer-projectile ibuffer-project ibuffer-git hl-todo hide-mode-line helpful haproxy-mode google-this git-timemachine git-modes git-messenger git-gutter-fringe flyspell-correct flycheck-plantuml flycheck-eglot eyebrowse expand-region evil eshell-git-prompt engine-mode embark-consult ellama eldoc-box drag-stuff doom-themes doom-modeline dockerfile-mode dired-sidebar dimmer diminish diff-at-point define-word dashboard csv-mode csproj-mode crux corfu consult-yasnippet consult-flyspell consult-flycheck consult-eglot consult-dir company-posframe company-box color-identifiers-mode cmake-mode citeproc-org chatgpt-shell cape browse-kill-ring bongo bm beacon backup-walker anzu all-the-icons-nerd-fonts all-the-icons-ibuffer all-the-icons-dired all-the-icons-completion aggressive-indent))
+ '(safe-local-variable-values
+   '((eval setq-local org-roam-db-location
+	   (expand-file-name ".org-roam.db" org-roam-directory))
+     (eval setq-local org-roam-directory
+	   (expand-file-name
+	    (locate-dominating-file default-directory ".dir-locals.el"))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
